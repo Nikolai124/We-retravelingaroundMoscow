ad1c99c244e1475cc96960543b5b08b8f437f114
Nikolai124 <g4728281919@gmail.com>
2026-09-14 19:28:52 +0300

    добавил приложение

--- WeretravelingaroundMoscow/settings.py
+++ WeretravelingaroundMoscow/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "places",
 ]
 
 MIDDLEWARE = [
@@ -116,13 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "core", "static"),
+os.path.join(BASE_DIR, "static"),
 ]
